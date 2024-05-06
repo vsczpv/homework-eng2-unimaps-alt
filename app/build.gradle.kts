db2e27454cacca9865852fac4a2cc44520f4b8d2
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-05-06 11:27:07 -0300

    Remove RatingBar dependency
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
+	testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
